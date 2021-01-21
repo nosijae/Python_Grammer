6b2e1a65c33e1e470454670963211fe5d255c244
nosijae <noh1816@hufs.ac.kr>
2021-01-21 16:59:47 +0900

    check

--- ForOfPython.py
+++ ForOfPython.py
@@ -1,3 +1,4 @@
+print("for 문")
 for i in range(1, 6):
     print("대기번호: {0}".format(i))
 print()
@@ -5,3 +6,5 @@ print()
 cafe = ["audu", "kim", "noh", "won"]
 for i in cafe:
     print("{0}, 커피가 준비되었습니다.".format(i))
+
+print("while 문")
